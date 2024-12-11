1b7df981bee07ab7394d2eb82626204298e2f3d6
Luis Santiago Duque Franco <lduquef@unal.edu.co>
2024-12-10 23:19:27 -0500

    cpp check debugger

--- 005_Tarea_1B/Src/CheckSourcesMain.c
+++ 005_Tarea_1B/Src/CheckSourcesMain.c
@@ -22,9 +22,14 @@ uint8_t leaking(uint16_t const range);
 uint8_t positiveFunction(uint8_t data);
 int dostuff(char *data, int value);
 uint8_t weirdFunction(uint8_t data);
-
+// defino variables para visualizar los resultados en express
 uint32_t result1;
 int16_t result2;
+uint8_t result3;
+uint8_t result4;
+uint8_t result5;
+
+int result6;
 /*
  * The main function, where everything happens.
  */
@@ -39,10 +44,10 @@ int main(void)
 	/* Probando diferentes funciones */
 	result1=checkRotation();
 	result2 = getTemperature(1);
-	leaking(350);
-	positiveFunction(2);
-	dostuff("abc", 1024);
-	weirdFunction(26);
+	result3 = leaking(7);
+	result4 = positiveFunction(2);
+	result6 = dostuff("abc",1024);
+	result5 = weirdFunction(26);
 
 	while (1) {
 	}
@@ -92,44 +97,83 @@ int16_t getTemperature(uint8_t getData){
 	return 0;
 }
 
-/* Describir cuales son los problemas que hay en esta función y como se pueden corregir */
+/* range debe ser < 10 , ya que si es mayor no se puede definir
+ * el char a[10]se sobre escribe y sale error, se soluciona aumentando el tamaño de a
+ * a[range] para evitar esto , y se debe retomar el valor
+ * a[range-1] ya que se cuenta desde el 0*/
 uint8_t leaking(uint16_t const range){
-	char a[10];
-
+//	char a[10];
+//	/* Utilice una linea de codigo del ciclo FOR y luego la otra, ¿que observa en la salida del cppcheck?*/
+//	for(uint8_t size = 0; size < range; size++){
+//	//for(uint8_t size = 0; size < 350; size++){
+//		a[size] = size*2;
+//	}
+//	/* Utilice una linea de codigo del ciclo FOR y luego la otra, ¿que observa en la salida del cppcheck?*/
+//	//return a[9];
+//	return a[10];
+	char a[255]={0};
 	/* Utilice una linea de codigo del ciclo FOR y luego la otra, ¿que observa en la salida del cppcheck?*/
+
 	for(uint8_t size = 0; size < range; size++){
-	//for(uint8_t size = 0; size < 350; size++){
-		a[size] = size*2;
-	}
-
-	/* Utilice una linea de codigo del ciclo FOR y luego la otra, ¿que observa en la salida del cppcheck?*/
-	//return a[9];
-	return a[10];
+		//for(uint8_t size = 0; size < 350; size++){
+			a[size] = size*2;
+		}
+		/* Utilice una linea de codigo del ciclo FOR y luego la otra, ¿que observa en la salida del cppcheck?*/
+		//return a[9];
+		return a[range-1];
 }
 
-/* Describir cuales son los problemas que hay en esta función y como se pueden corregir */
+/* la comparación data== 1 no permite saber si el  numero es positivo
+ * por lo que se debe usar data>0 */
 uint8_t positiveFunction(uint8_t data){
-	if(data == 1){
-		return 1;
-	}else{
-		return 1;
-	}
+//	if(data == 1){
+//		return 1;
+//	}else{
+//		return 1;
+//	}
+	if(data > 0){
+			return 1;
+		}else{
+			return 0;
+		}
 }
 
-/* Describir cuales son los problemas que hay en esta función y como se pueden corregir */
+/* la variable weird no se usa de manera adecuada
+ * elimiar el weird y arreglar el if para que la condición/verificación
+ * data == 25 tenga sentido
+ *
+ *  */
 uint8_t weirdFunction(uint8_t data){
-	uint8_t weird = 10;
-	if((weird = data) == 25){
+//	uint8_t weird = 10;
+//	if((weird = data) == 25){
+//		return 0;
+//	}else{
+//		return 0;
+//	}
+	if(data == 25){
 		return 0;
 	}else{
 		return 0;
 	}
 }
 
-/* Describir cuales son los problemas que hay en esta función y como se pueden corregir */
+/* data[125] está fuera del los limites definidos para data
+ * pudiendo causar desbordamiento de memoría
+ *definit data[value]
+ * */
 int dostuff(char *data, int value){
-	data[125] = 200;
-	dummy = value + 1;
+//	data[125] = 200;
+//	dummy = value + 1;
+//
+//	return 0;
+	if (value < 5) {
+	        data[value] = 200; // Acceso seguro
+	    } else {
+	        // Manejo de error: arreglo demasiado pequeño
+	        return 0;
+	    }
 
-	return 0;
+	    dummy = value + 1;
+
+	    return 1;
 }
